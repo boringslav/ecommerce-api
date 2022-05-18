d8108d63c17b01be35e0f2491c39d205c77ec82d
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-05-18 10:13:20 +0200

    Test GetAllProducts endpoint

--- src/product/test/integration/product.service.int-spec.ts
+++ src/product/test/integration/product.service.int-spec.ts
@@ -18,6 +18,10 @@ describe('ProductService Integration', () => {
     await repository.cleanDatabase();
   });
 
+  beforeEach(async () => {
+    await repository.cleanDatabase();
+  });
+
   describe('Create Product', () => {
     it('Should create Product', async () => {
       const productDTO: ProductDto = {
@@ -50,6 +54,41 @@ describe('ProductService Integration', () => {
       expect(product.price).toBe(productDTO.price);
       expect(product.image).toBe(productDTO.image);
       expect(product.description).toBe(productDTO.description);
+    });
+  });
+
+  describe('Get Products', () => {
+    it('Should return an empty array if there are no products', async () => {
+      const products = await productService.getAllProducts();
+      expect(products).toEqual([]);
+    });
+    it('Should return an array of products', async () => {
+      const product1: ProductDto = {
+        name: 'BMW e92 335I',
+        description: 'Super fast borkomobile',
+        price: 15000,
+        image:
+          'https://media.schmiedmann.com/media/79312/bmw_335i_e92_by_schmiedmann_finland_hdr03.jpg?maxwidth=1024&maxheight=768',
+      };
+      const product2: ProductDto = {
+        name: 'BMW  330d',
+        description: 'Dizel begachka nema',
+        price: 15000,
+        image:
+          'https://cloud.leparking.fr/2021/07/26/04/08/bmw-serie-3-2014-bmw-330d-msport-for-sale-in-sligo-for-21-950-on-donedeal-bleu_8218059308.jpg',
+      };
+
+      await productService.createProduct(product1);
+      await productService.createProduct(product2);
+
+      const products = await productService.getAllProducts();
+      expect(products.at(0).name).toBe(product1.name);
+      expect(products.at(0).description).toBe(product1.description);
+      expect(products.at(0).price).toBe(product1.price);
+
+      expect(products.at(1).name).toBe(product2.name);
+      expect(products.at(1).description).toBe(product2.description);
+      expect(products.at(1).price).toBe(product2.price);
     });
   });
 });
