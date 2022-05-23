65a9f1a6c9ab642086e427b5834353bf024957f1
Borislav Stoyanov <borislav.stoyanov@musala.com>
2022-05-23 13:20:02 +0200

    Generate order module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ProductModule } from './product/product.module';
 import { RepositoryModule } from './repository/repository.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
     ProductModule,
     RepositoryModule,
     UserModule,
+    OrderModule,
   ],
   controllers: [],
   providers: [],
